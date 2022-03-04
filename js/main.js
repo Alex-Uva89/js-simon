938bb8b27cc38a1da5350acf220fbdc4cbf27ea3
ALESSANDROUVA <a.uva1988@gmail.com>
2022-03-04 15:39:54 +0100

    Pseudocodice 2

--- js/main.js
+++ js/main.js
@@ -6,17 +6,23 @@
 
 // PSEUDOCODICE
 
-// 1 - Visualizzare in pagina 5 numeri casuali. (for per creare 5 numeri)
+// CONTENUTO HTML:
+
+const displayNumberRandom = document.getElementById('card_number_random');
+const displayWinningNumbers = document.getElementById('result');
+
+
+// 1 - Visualizzare in pagina 5 numeri casuali. (for per creare 5 numeri) 
 //     1.1 - creare i numeri casuali.
 //     1.2 - creo un array dove inserire i numeri casuali (anche doppi) (numeriCasuali)
-//     1.3 - visualizzare i numeriCasuali per 30 secondi.
+//     1.3 - visualizzare i numeriCasuali per 30 secondi. °°°°°CONTENUTO HTML°°°°°
 // 2 - Da lì parte un timer di 30 secondi. alla fine del timer nascondo i numeri prensenti in numeriCasuali.
 // 3- chiedi all'utente (con dei prompt) di inserirli in ORDINE, uno alla volta. (for per chiedere 5 numeri)
-//     3.1 - chiedo all'utente un numero 5 volte di seguito
+//     3.1 - chiedo all'utente un numero 5 volte di seguito °°°°°CONTENUTO HTML°°°°°
 //     3.2 - inserisco ogni numero che mi da l'utente in un array utente (numeriUtente)
 // 4- il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 //     4.1 - ogni numero dell array numeriUtente viene controllato all'interno di numeriCasuali
 //     4.2 - creo un array 'vittoria'
 //     4.3 - i numeri uguali di numeriCasuali e numeriUtente vengono inseriti in 'vittoria'
-//     4.4 - viene mostrato il contenuto dell'array 'vittoria'.
-// BONUS SE RIESCO 4.5 - SE il box vittoria contiene 5 elementi compare il messaggio 'hai vinto'
+//     4.4 - viene mostrato il contenuto dell'array 'vittoria'. °°°°°CONTENUTO HTML°°°°°
+// BONUS SE RIESCO 4.5 - SE il box vittoria contiene 5 elementi compare il messaggio 'hai vinto' °°°°°CONTENUTO HTML°°°°°
