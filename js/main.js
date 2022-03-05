2551ba947173bf22d7764994b9278266853156cf
ALESSANDROUVA <a.uva1988@gmail.com>
2022-03-05 12:29:20 +0100

    blocco richiesta numeri

--- js/main.js
+++ js/main.js
@@ -12,20 +12,20 @@ const displayNumberRandom = document.getElementById('card_number_random');
 const displayWinningNumbers = document.getElementById('result');
 const btnPlay = document.getElementById('play');
 
-let numberPcRandom; // creo array da riempire per i numeri pc casuali
-let numberUse; // creo array da riempire con i numeri del prompt dati dall'user 
-let numberWinners; // creo array da riempire con i numeri dati dalla funzione di controllo
+// devo dichiararli da subito array altrimenti in alcune funzioni non possono funzionare
+let numberPcRandom = []; // creo array da riempire per i numeri pc casuali
+let numberUser = []; // creo array da riempire con i numeri del prompt dati dall'user 
+let numberWinners = []; // creo array da riempire con i numeri dati dalla funzione di controllo
 
-let removeNumberTimer = 30000; // inserisco variabile di tempo per far sparire i numeri
+let removeNumberTimer = 5000; // inserisco variabile di tempo per far sparire i numeri
 let askNumberPromptTimer = 1000; // inserisco variabile per far partire il prompt
-
-
-
 
 // :::::::::::::: 1 - Visualizzare in pagina 5 numeri casuali. (for per creare 5 numeri)::::::::::
 
 generateNumberPcRandom(5);
 console.log(numberPcRandom);
+printNumber(); // inserisco il comando che fa attivare la funzione della stampa numeri;
+let timerRemove = setInterval(removeNumber, removeNumberTimer); //creo timer per far rimuovere i numeri con all'interno il timer per far partire il prompt
 
 
 function calcRandomNumber(min, max) { // creo funzione per creare numeri randomici
@@ -44,19 +44,17 @@ function generateNumberPcRandom(num) { //creo funzione per
     return numberPcRandom;
 }
 
-printNumber(); // inserisco il comando che fa attivare la funzione della stampa numeri;
 
 function printNumber() { // creo funzione per la stampa dei numeri
-    numeri.innerHTML = `<h2 class="number">randomNumber</h2>`;
+    displayNumberRandom.innerHTML = `<h2 class="number">${numberPcRandom}</h2>`;
 }
 
 
 // ::::::::::::::::: 2 - Da lì parte un timer di 30 secondi, alla fine spariscono. ::::::::::::::::
-let timerRemove = setInterval(removeNumber, removeNumberTimer); //creo timer per far rimuovere i numeri con all'interno il timer per far partire il prompt
 
 function removeNumber () {
-    displayNumberRandom.innerHTML = '';
-    clearInterval(removeNumberTimer);
+    displayNumberRandom.innerHTML = ''; //la funzione parte riempiendo l'htm con uno spazio vuoto
+    clearInterval(removeNumberTimer); //questa funzione essendo interval necessita di clear
     setTimeout(askNumberAtUser, askNumberPromptTimer); // creo il timer per far partire la richiesta dei numeri e insieme li controlla
 }
 
@@ -65,9 +63,19 @@ function removeNumber () {
 //     3.2 - inserisco ogni numero che mi da l'utente in un array utente (numeriUtente)
 //     3.3 - il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
-
+function askNumberAtUser() {
+    for(let i = 0; numberUser.length < numberPcRandom.length; i++) {
+        let inputUser = parseInt(prompt('inserisci i numeri nella sequenza vista, numero ' + (i+1) + ':'))
+        if (numberUser.includes(inputUser)) {
+            inputUser = parseInt(prompt('I numeri inseriti devono essere diversi, reinserisci il numero ' + (i+1) + ':'))
+        }
+        numberUser.push(inputUser)
+    }
+    console.log(numberUser)
+}
 
 //::::::::::: 4 - ogni numero dell array numeriUtente viene controllato all'interno di numeriCasuali
+
 
 
 //::::::::::: 5 - vengono dichiarati i numeri indovinati
