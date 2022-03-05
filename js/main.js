ab37932e2968308ca7c31673102e4a7b78405c6b
ALESSANDROUVA <a.uva1988@gmail.com>
2022-03-05 11:44:53 +0100

    add

--- js/main.js
+++ js/main.js
@@ -63,16 +63,11 @@ function removeNumber () {
 //::::::::::: 3- chiedi all'utente (con dei prompt) di inserirli in ORDINE, uno alla volta.
 //     3.1 - chiedo all'utente un numero 5 volte di seguito °°°°°CONTENUTO HTML°°°°°
 //     3.2 - inserisco ogni numero che mi da l'utente in un array utente (numeriUtente)
+//     3.3 - il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
 
 
-// 4- il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+//::::::::::: 4 - ogni numero dell array numeriUtente viene controllato all'interno di numeriCasuali
 
 
-
-//     4.1 - ogni numero dell array numeriUtente viene controllato all'interno di numeriCasuali
-
-//     4.2 - creo un array 'vittoria'
-//     4.3 - i numeri uguali di numeriCasuali e numeriUtente vengono inseriti in 'vittoria'
-//     4.4 - viene mostrato il contenuto dell'array 'vittoria'. °°°°°CONTENUTO HTML°°°°°
-// BONUS SE RIESCO 4.5 - SE il box vittoria contiene 5 elementi compare il messaggio 'hai vinto' °°°°°CONTENUTO HTML°°°°°
+//::::::::::: 5 - vengono dichiarati i numeri indovinati
